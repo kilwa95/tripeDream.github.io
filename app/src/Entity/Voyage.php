a6717bc668bf8f3bb1eccd1ca8161a9cd13ffa27
Khaled Abdulhalim <lga.sig.khaled@gmail.com>
2020-11-08 16:09:56 +0100

    table voyage

---
+++ app/src/Entity/Voyage.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoyageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VoyageRepository::class)
+ */
+class Voyage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $pointFort;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPointFort(): ?string
+    {
+        return $this->pointFort;
+    }
+
+    public function setPointFort(?string $pointFort): self
+    {
+        $this->pointFort = $pointFort;
+
+        return $this;
+    }
+}
