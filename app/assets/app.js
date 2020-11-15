de177be52c641a70c9c453d6628753b44f201b82
Khaled Abdulhalim <lga.sig.khaled@gmail.com>
2020-11-15 21:33:14 +0100

    landing homepage

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,2 +1,27 @@
+import './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 import 'bootstrap';
+ import $ from 'jquery';
+
+if ($(window).width() > 992) {
+    $(window).scroll(function(){  
+       if ($(this).scrollTop() > 40) {
+          $('#navbar_top').addClass("fixed-top");
+          // add padding top to show content behind navbar
+          $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+        }else{
+          $('#navbar_top').removeClass("fixed-top");
+           // remove padding top from body
+          $('body').css('padding-top', '0');
+        }   
+    });
+  } // end if
+
+
+  $(document).on('click', '.dropdown-menu', function (e) {
+    e.stopPropagation();
+  });
+
+  $(".animated").addClass("delay-1s");
---
+++ app/src/Controller/NavigationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NavigationController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('navigation/homepage.html.twig');
+    }
+}
