1c85b5a1a5dcd932a4b3dcb96530175841fbeadb
Khaled Abdulhalim <lga.sig.khaled@gmail.com>
2020-11-28 21:27:41 +0100

    rechercher-voyage-templete

--- app/src/Controller/VoyageController.php
+++ app/src/Controller/VoyageController.php
@@ -5,16 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ActiviteRepository;
+use App\Repository\PaysRepository;
+use App\Repository\SaisonRepository;
 
 class VoyageController extends AbstractController
 {
     /**
      * @Route("/voyage", name="voyage")
      */
-    public function index(): Response
+    public function index(ActiviteRepository $ActiviteRepository,PaysRepository $PaysRepository
+    ,SaisonRepository $SaisonRepository)
     {
-        return $this->render('voyage/index.html.twig', [
-            'controller_name' => 'VoyageController',
+        $activites = $ActiviteRepository->findAll();
+        $pays = $PaysRepository->findAll();
+        $saison= $SaisonRepository->findAll();
+
+        
+        return $this->render('voyage/index.html.twig',[
+            'activites' => $activites,
+            'pays' => $pays,
+            'saison' =>  $saison
         ]);
     }
 }
