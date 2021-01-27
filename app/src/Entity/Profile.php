c02abd13601d6d4a8a1ff17f80f3628ff389a3b7
sassank <sassan.khalafi@gmail.com>
2021-01-27 06:29:00 +0100

    Entité du profil

---
+++ app/src/Entity/Profile.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProfileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProfileRepository::class)
+ */
+class Profile
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+   
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="Profile")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Voyage::class, inversedBy="Profile")
+     */
+    private $voyage;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+  
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getVoyage(): ?Voyage
+    {
+        return $this->voyage;
+    }
+
+    public function setVoyage(?Voyage $voyage): self
+    {
+        $this->voyage = $voyage;
+
+        return $this;
+    }
+}
