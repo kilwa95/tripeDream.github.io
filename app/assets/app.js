5cb2f467734ca74e368e6c597a99a4848d58487d
khaled <lga.sig.khaled@gmail.com>
2021-02-19 00:23:55 +0100

    best-voyage

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,6 +1,7 @@
 import './styles/app.css';
 import './styles/menu.css';
 import './styles/slides.css';
+import './styles/best-voyage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './custom-js/custom-app.js';
