50bf3a4188e0a5770de7ae538587a5145a3152bc
khaled <lga.sig.khaled@gmail.com>
2021-02-19 23:57:15 +0100

    footer

--- app/assets/app.js
+++ app/assets/app.js
@@ -3,6 +3,7 @@ import './styles/menu.css';
 import './styles/slides.css';
 import './styles/best-voyage.css';
 import './styles/idee-voyage.css';
+import './styles/footer.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
