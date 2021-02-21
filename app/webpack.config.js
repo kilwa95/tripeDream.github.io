a1d5442db95bebd144feb2f4a9851941c4fa1bce
khaled <lga.sig.khaled@gmail.com>
2021-02-21 15:11:58 +0100

    login-ui

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -24,6 +24,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
 	.addEntry('app', './assets/app.js')
+	.addEntry('security', './assets/security.js')
 	//.addEntry('page2', './assets/page2.js')
 
 	// When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
