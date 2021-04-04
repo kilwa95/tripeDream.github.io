183180228495d612c5ad3c31d16e88cec36d7d89
khaled <lga.sig.khaled@gmail.com>
2021-04-04 14:16:36 +0200

    ajouter-au-panier

--- app/src/Controller/Front/PanierController.php
+++ app/src/Controller/Front/PanierController.php
@@ -3,8 +3,13 @@
 namespace App\Controller\Front;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Panier;
+use App\Repository\VoyageRepository;
+
+
 
 
 /**
@@ -18,5 +23,22 @@ class PanierController extends AbstractController
     public function index(): Response
     {
         return $this->render('Front/panier/index.html.twig');
+    }
+    /**
+     * @Route("/new/{id}", name="panier_new", methods={"GET","POST"})
+     * @IsGranted("ROLE_USER")
+     */
+    public function new(int $id, VoyageRepository $voyageRepository): Response
+    {
+        $panier= new Panier();
+        $voyage = $voyageRepository->find($id);
+        $panier->setVoyage($voyage);
+        $user = $this->getUser();
+        $user->addPanier($panier);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($panier);
+        $entityManager->flush();
+       
+        return $this->redirectToRoute('voyage_show', ['id'=> $id]);
     }
 }
