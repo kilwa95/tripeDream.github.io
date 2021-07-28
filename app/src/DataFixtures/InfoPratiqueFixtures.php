f502bc3871af878dfb54cd1dd26a4acb211effb3
aza93 <a.azabar93@gmail.com>
2021-07-29 01:01:15 +0200

    Update fixtures

--- app/src/DataFixtures/InfoPratiqueFixtures.php
+++ app/src/DataFixtures/InfoPratiqueFixtures.php
@@ -14,8 +14,8 @@ class InfoPratiqueFixtures extends Fixture
         $faker = \Faker\Factory::create('FR-fr');
         for ($i = 0; $i < 100; $i++) {
             $infoPratique  = new InfoPratique();
-            $infoPratique->setRendezVous($faker->dateTime('Y-m-d'));
-            $infoPratique->setFinSejour($faker->dateTime('Y-m-d'));
+            $infoPratique->setRendezVous($faker->dateTimeBetween("now", '+'.rand(1, 5).' days'));
+            $infoPratique->setFinSejour($faker->dateTimeBetween($infoPratique->getRendezVous(), '+'.rand(6, 60).' days'));
             $infoPratique->setHebergement($faker->realText());
             $infoPratique->setRepas($faker->realText());
             $infoPratique->setCovid19($faker->realText());
